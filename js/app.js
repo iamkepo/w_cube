a6485cdcb9835dd3d71149f6bda6649fdaac9283
iamkepo <christamour.kakpo@gmail.com>
2022-05-10 10:05:26 +0200

    change image

--- js/app.js
+++ js/app.js
@@ -1,5 +1,25 @@
 let scene, camera, renderer;
+let select_list = document.querySelector(".select-list");
+let tapon = document.querySelector(".tampon");
+let img = localStorage.getItem("img") === null ? "arid" : localStorage.getItem("img");
+tapon.innerText = img + " ( courant )";
+tapon.addEventListener("click", e =>{
+  select_list.style.height = "100%";
+  select_list.style.overflowY = "auto";
 
+  let select = document.querySelectorAll(".select");
+  for (let i = 0; i < select.length; i++) {
+    if (select[i].innerText === img) {
+      select[i].style.display = "none";
+    }
+    select[i].addEventListener("click", e =>{
+      localStorage.setItem("img", select[i].innerText);
+      window.location.reload();
+    })
+  
+  }
+})
+  init();
 function init() {
   scene = new THREE.Scene();
   camera = new THREE.PerspectiveCamera(55,window.innerWidth/window.innerHeight,45,30000);
@@ -7,7 +27,7 @@ function init() {
 
   renderer = new THREE.WebGLRenderer({antialias:true});
   renderer.setSize(window.innerWidth,window.innerHeight);
-  document.body.appendChild(renderer.domElement);
+  document.getElementById('app').appendChild(renderer.domElement);
 
   let controls = new THREE.OrbitControls( camera, renderer.domElement );
   controls.addEventListener('change', renderer.domElement);
@@ -15,19 +35,21 @@ function init() {
   //controls.maxDistance = 1500;
   
   let materialArray = [];
-  let texture_ft = new THREE.TextureLoader().load( './img/mystic_ft.jpg');
-  let texture_bk = new THREE.TextureLoader().load( './img/mystic_bk.jpg');
-  let texture_up = new THREE.TextureLoader().load( './img/mystic_up.jpg');
-  let texture_dn = new THREE.TextureLoader().load( './img/mystic_dn.jpg');
-  let texture_rt = new THREE.TextureLoader().load( './img/mystic_rt.jpg');
-  let texture_lf = new THREE.TextureLoader().load( './img/mystic_lf.jpg');
+  let texture = {
+    ft : new THREE.TextureLoader().load( './img/'+img+'_ft.jpg'),
+    bk : new THREE.TextureLoader().load( './img/'+img+'_bk.jpg'),
+    up : new THREE.TextureLoader().load( './img/'+img+'_up.jpg'),
+    dn : new THREE.TextureLoader().load( './img/'+img+'_dn.jpg'),
+    rt : new THREE.TextureLoader().load( './img/'+img+'_rt.jpg'),
+    lf : new THREE.TextureLoader().load( './img/'+img+'_lf.jpg'),
+  }
     
-  materialArray.push(new THREE.MeshBasicMaterial( { map: texture_ft }));
-  materialArray.push(new THREE.MeshBasicMaterial( { map: texture_bk }));
-  materialArray.push(new THREE.MeshBasicMaterial( { map: texture_up }));
-  materialArray.push(new THREE.MeshBasicMaterial( { map: texture_dn }));
-  materialArray.push(new THREE.MeshBasicMaterial( { map: texture_rt }));
-  materialArray.push(new THREE.MeshBasicMaterial( { map: texture_lf }));
+  materialArray.push(new THREE.MeshBasicMaterial( { map: texture.ft }));
+  materialArray.push(new THREE.MeshBasicMaterial( { map: texture.bk }));
+  materialArray.push(new THREE.MeshBasicMaterial( { map: texture.up }));
+  materialArray.push(new THREE.MeshBasicMaterial( { map: texture.dn }));
+  materialArray.push(new THREE.MeshBasicMaterial( { map: texture.rt }));
+  materialArray.push(new THREE.MeshBasicMaterial( { map: texture.lf }));
 
   for (let i = 0; i < 6; i++)
       materialArray[i].side = THREE.BackSide;
@@ -40,4 +62,4 @@ function animate() {
   renderer.render(scene,camera);
   requestAnimationFrame(animate);
 }
-init();
+
