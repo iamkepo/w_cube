1d63b509de93c9611afa99f130fa94f597d428f7
Christ-Amour KAKPO <81099224+iamkepo@users.noreply.github.com>
2022-12-29 11:52:55 +0100

    Update app.js

--- js/app.js
+++ js/app.js
@@ -31,8 +31,8 @@ function init() {
 
   let controls = new THREE.OrbitControls( camera, renderer.domElement );
   controls.addEventListener('change', renderer.domElement);
-  //controls.minDistance = 500;
-  //controls.maxDistance = 1500;
+  controls.minDistance = 500;
+  controls.maxDistance = 1500;
   
   let materialArray = [];
   let texture = {
